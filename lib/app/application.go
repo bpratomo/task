cd835ad44f5618e6d88cc76c94634a9bb833404a
Budi Pratomo <buddy.pratomo@gmail.com>
2023-06-11 16:23:38 +0200

    added task creation in UI

--- lib/app/application.go
+++ lib/app/application.go
@@ -4,24 +4,30 @@ import (
 	"fmt"
 
 	c "task/lib/app/components"
+	r "task/lib/controllers"
 	d "task/lib/database"
+	m "task/lib/models"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 var app *tview.Application
-var inputMode = false
+var flex *tview.Flex
+var displayedTasks []m.Task
+var inputField *tview.InputField
+var list *tview.List
 
 func Run() {
 
 	app = tview.NewApplication()
-	tasks := d.Get("")
-	list := c.RenderListBox(app, tasks)
-	inputField := c.RenderSearchBox(app, tasks)
+	displayedTasks = d.Get("")
 
-	flex := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(inputField, 0, 1, true).
+	list = c.ConfigureTaskList(app, displayedTasks)
+	inputField = c.RenderSearchBox(app, updateTaskList(), submitTask())
+
+	flex = tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(inputField, 3, 1, true).
 		AddItem(list, 0, 10, false)
 
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
@@ -31,11 +37,10 @@ func Run() {
 			handleMovement(event.Key(), flex, app)
 			return nil
 
-		case tcell.KeyCtrlC:
-			app.Stop()
-		}
+		default:
+			return event
 
-		return event
+		}
 
 	})
 
@@ -44,6 +49,30 @@ func Run() {
 	}
 
 	fmt.Println(app)
+}
+
+func updateTaskList() func(string) {
+	return func(s string) {
+		displayedTasks = d.Get(s)
+		c.ReRenderList(list, displayedTasks)
+	}
+}
+
+func submitTask() func(string) {
+	return func(s string) {
+		r.Create([]string{s})
+		updateTaskList()("")
+		app.SetFocus(list)
+		app.SetFocus(flex)
+
+	}
+}
+
+func focusOnTasks() {
+	if flex == nil {
+		return
+	}
+	app.SetFocus(flex.GetItem(1))
 }
 
 func handleMovement(k tcell.Key, flex *tview.Flex, app *tview.Application) {
@@ -55,12 +84,11 @@ func handleMovement(k tcell.Key, flex *tview.Flex, app *tview.Application) {
 			break
 		}
 	}
-
 	var toBeFocusedIndex int
 	switch k {
 	case tcell.KeyRight:
 		if focusedIndex < flex.GetItemCount()-1 {
-            toBeFocusedIndex = focusedIndex + 1
+			toBeFocusedIndex = focusedIndex + 1
 		} else {
 			toBeFocusedIndex = 0
 		}
--- lib/app/components/tasklist.go
+++ lib/app/components/tasklist.go
@@ -14,28 +14,29 @@ var selectedIndex int
 var curList *tview.List
 
 func ConfigureTaskList(app *tview.Application, tasks []m.Task) *tview.List {
-	list := RenderList(app, tasks)
-	curList = list
-	list.SetBorder(true).SetTitle("Task list").SetTitleAlign(tview.AlignLeft)
-	list.SetChangedFunc(selectionFunc)
-	list.SetInputCapture(handleKeyInput)
-	list.SetSelectedFocusOnly(true)
-	list.ShowSecondaryText(false)
+	curList = tview.NewList()
+	RenderList(curList, tasks)
+	curList.SetBorder(true).SetTitle("Task list").SetTitleAlign(tview.AlignLeft)
+	curList.SetChangedFunc(selectionFunc)
+	curList.SetInputCapture(handleKeyInput)
+	curList.SetSelectedFocusOnly(true)
+	curList.ShowSecondaryText(false)
 
-	return list
+	return curList
 
 }
 
-func RenderList(app *tview.Application, tasks []m.Task) *tview.List {
-	list := tview.NewList()
+func ReRenderList(list *tview.List, tasks []m.Task) {
+	list.Clear()
+	RenderList(list, tasks)
+}
 
+func RenderList(list *tview.List, tasks []m.Task) {
 	for i, task := range tasks {
 		istr := strconv.Itoa(i + 1)
 		irunes := []rune(istr)
 		list.AddItem(task.Title, strconv.Itoa(task.ID), irunes[0], nil)
 	}
-
-	return list
 }
 
 func selectionFunc(index int, mainText string, secondaryText string, shortcut rune) {
